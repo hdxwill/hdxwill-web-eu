2b7714582121789d62974778457a63cd08f23fda
biwoo-hdxwill <140477718+biwoo-hdxwill@users.noreply.github.com>
2026-03-18 13:33:24 +0900

    feat: home - dental soultions

--- src/components/home/DentalSolutions.jsx
+++ src/components/home/DentalSolutions.jsx
@@ -7,11 +7,14 @@ const DentalSolutions = () => {
       <div className="container">
         <div className="solutions-grid">
           <div className="solutions-image">
-            <img src="/images/home/laptop.png" alt="Dental Solutions" />
+            <img
+              src="/images/home/dental_solutions.png"
+              alt="Dental Solutions"
+            />
           </div>
           <div className="solutions-text">
-            <h2 className="section-title text-black">Dental Solutions</h2>
-            <p className="section-desc">
+            <h2 className="solutions-title">Dental Solutions</h2>
+            <p className="solutions-desc">
               Our company provides <strong>advanced dental solutions</strong>{" "}
               driven by <strong>cutting-edge X-ray imaging technology</strong>.
               With{" "}
